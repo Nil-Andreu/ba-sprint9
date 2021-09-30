05de5e1692f0438db23946c03f27204e3bd2cbcb
Nil Andreu <nilandreug@gmail.com>
2021-09-30 22:11:04 +0200

    Fetch data from the api done

--- src/components/PrincipalPage.jsx
+++ src/components/PrincipalPage.jsx
@@ -1,87 +1,153 @@
-import React, {Fragment, useEffect, useState} from 'react'
-import styled from 'styled-components'
-import axios from 'axios' 
-import logo from '../assets/logo.svg'
+import React, { Fragment, useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import logo from "../assets/logo.svg";
 
 function PrincipalPage() {
-    // We create the function with axios
-    const FetchUrl = async (url) => {
-        let data = await axios(url)
-        return data
+  const [url, setUrl] = useState("https://swapi.dev/api/starships/");
+  const [buttonList, setButtonList] = useState(false);
+  const [data, setData] = useState([]);
+
+  let buttonListHandler = (number) => {
+    if (number == 1) {
+      setButtonList(false);
+    } else {
+      setButtonList(true);
     }
-    const [url, setUrl] = useState("https://swapi.dev/api/starships/")
+  };
 
-    useEffect(() => {
-        let data = FetchUrl(url)
-        console.log(data)
-    }, [])
+  useEffect(() => {
+    // We create the function with axios
+    const FetchUrl = async (urlQuery) => {
+      let result = await axios(urlQuery);
+      setData(result.data.results);
+    }
+    FetchUrl(url);
+  }, [url]);
 
-
-    return (
-        <Fragment>
-            <Header>
-                <Image src={logo} />
-            
-            </Header>
-            <Navbar>
-                <Button>
-                    Home
-                </Button>
-                <Button>
-                    Starthips
-                </Button>
-            </Navbar>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Header>
+        <Image src={logo} />
+      </Header>
+      <Navbar>
+        <Button
+          checked={buttonList}
+          onClick={() => {
+            buttonListHandler(1);
+          }}
+        >
+          Home
+        </Button>
+        <Button
+          checked={buttonList}
+          onClick={() => {
+            buttonListHandler(2);
+          }}
+        >
+          Starthips
+        </Button>
+      </Navbar>
+      {buttonList ? (
+        <List>
+          {data.map((i) => {
+              {console.log(i.name)}
+            <ListCard key={i.name}>
+              <ListCardTitle>{i.name}</ListCardTitle>
+              <ListCardType>{i.model}</ListCardType>
+            </ListCard>
+          })}
+        </List>
+      ) : (
+        <HomePage />
+      )}
+    </Fragment>
+  );
 }
 
 const Header = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 0 0 0 0;
-    justify-content: center;
-    align-items: center;
-    height: 20vh;
-    width: 100%;
-    background-color: #000;
+  display: flex;
+  flex-direction: row;
+  margin: 0 0 0 0;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+  width: 100%;
+  background-color: #000;
 `;
 
 const Image = styled.img`
-    justify-self: center;
-    height: 19vh;
+  justify-self: center;
+  height: 19vh;
 `;
 
-
 const Navbar = styled.div`
-    border: white;
-    height: 10vh;
-    background-color: #000;
-    border-top: 1px solid #aaaaaa;
-    border-bottom: 1px solid #aaaaaa;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+  border: white;
+  height: 10vh;
+  background-color: #000;
+  border-top: 1px solid #aaaaaa;
+  border-bottom: 1px solid #aaaaaa;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 const Button = styled.button`
-    width: 10vw;
-    height: 100%;
-    border-left: 1px solid #aaaaaa;
-    border-right: 1px solid #aaaaaa;
-    color:#999999;
+  width: 10vw;
+  height: 100%;
+  border-left: 1px solid #aaaaaa;
+  border-right: 1px solid #aaaaaa;
+  color: #999999;
 
-    border-top: none;
-    border-bottom: none;
-    background-color: transparent;
+  border-top: none;
+  border-bottom: none;
+  background-color: transparent;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
 
-    &:focus {
-        border-bottom: 7px solid #2a58f1;
-    }
+  &:focus {
+    border-bottom: 7px solid #2a58f1;
+  }
 `;
 
-export default PrincipalPage
+const HomePage = styled.div`
+  width: 100%;
+  height: 80vh;
+`;
+
+const List = styled.div`
+  background-color: #000;
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const ListCard = styled.div`
+  margin: 4vh 5vw;
+  height: 14vh;
+  border: 1px solid #aaa;
+  width: 90vw;
+  background-color: #4e4d4d;
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const ListCardTitle = styled.h2`
+  font-size: 2rem;
+  color: #999999;
+  text-transform: uppercase;
+`;
+
+const ListCardType = styled.h3`
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  color: #999999;
+`;
+
+export default PrincipalPage;
