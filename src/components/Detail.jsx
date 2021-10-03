3a697270f8fb1c26f63d47e291f14c8092aa8681
Nil Andreu <nilandreug@gmail.com>
2021-10-03 10:14:27 +0200

    Bugs with fetch

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Detail() {
     let url = "https://swapi.dev/api/starships/"
+    const [data, setData] = useState([])
 
   // Now we obtain the name of the startship we clicked the name of
   useEffect(() => {
@@ -16,11 +17,23 @@ function Detail() {
     // And create the new url for the query
     let urlQuery = url + id +  "/"
     console.log(urlQuery)
+    urlQuery = "https://swapi.dev/api/starships/9/"
 
-    const QueryStarship = async (urlQuery) => {
-        let query = await axios(urlQuery)
-        console.log(query)
+    // Now let's make the query with fetch
+    let QueryStarship = async(urlQuery) => {
+      let query = await fetch(urlQuery, {
+        method: "GET",
+        Headers: "application/json"
+      })
+      .then(res => res.text()) // Transforming the data to json
+      .then(res => console.log(res))
+      .catch(error => console.log(error)) // And some error handling when we are not able to request the detail for the given starship
+      
+      setData(query)
     }
+    // fetch(url).then((data) => console.log(data))
+
+    
 
     QueryStarship()
   }, []);
