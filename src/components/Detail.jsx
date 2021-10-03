bdb393c2d1b8a21c278ad59e0859681ace64cbd7
Nil Andreu <nilandreug@gmail.com>
2021-10-03 18:46:26 +0200

    Exercise 2 fetch done

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,43 +2,23 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Detail() {
-  /*let url = "https://swapi.dev/api/starships/";
   const [data, setData] = useState([]);
 
-  // Now we obtain the name of the startship we clicked the name of
   useEffect(() => {
     let queryString = window.location.search;
-    console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
-    console.log(urlParams);
     let id = urlParams.get("id");
-    console.log(id);
 
-    // And create the new url for the query
-    let urlQuery = url + id + "/?format=json";
-    console.log(urlQuery.toString());
-
-    // Now let's make the query with fetch
-    let QueryStarship = async (urlQuery) => {
-      await fetch(urlQuery, {
-        headers: {
-          type: "application/json",
-        },
-      })
-        .then((query) => console.log(query))
-        .then((query) => setData(query));
-    };
-    // fetch(url).then((data) => console.log(data))
-
-    QueryStarship();
-  }, []);*/
-
-  useEffect(() => {
     // We create the function with axios
-    const FetchUrl = async () => {
-      let result = await axios.get("https://swapi.dev/api/starships/2/?format=json")
-      result = result.data
+    const FetchUrl = async (urlQuery) => {
+      let result = await axios.get(`https://swapi.dev/api/starships/${id}/?format=json`).then(
+        res => res.data
+      ).catch( // For catching the errors
+        err => console.log(err)
+      )
       console.log(result)
+      setData(result)
+      console.log(data)
     };
     FetchUrl();
   }, []);
