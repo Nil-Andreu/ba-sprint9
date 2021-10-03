8a564d0bf797339ec958de645db2247daed0cc46
Nil Andreu <nilandreug@gmail.com>
2021-10-03 18:58:56 +0200

    Error handling for exercise 2

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import styled from 'styled-components'
 import axios from "axios";
 
 function Detail() {
   const [data, setData] = useState([]);
+  const [err, setErr] = useState(false)
 
   useEffect(() => {
     let queryString = window.location.search;
@@ -10,20 +12,43 @@ function Detail() {
     let id = urlParams.get("id");
 
     // We create the function with axios
-    const FetchUrl = async (urlQuery) => {
+    const FetchUrl = async () => {
       let result = await axios.get(`https://swapi.dev/api/starships/${id}/?format=json`).then(
         res => res.data
       ).catch( // For catching the errors
-        err => console.log(err)
+        error => {setErr(error.toString())
+        console.log(error)}
+        
       )
-      console.log(result)
       setData(result)
-      console.log(data)
     };
+
     FetchUrl();
   }, []);
 
-  return <div></div>;
+  return <Fragment>
+    <Container>
+      {(!err) ? <DetailInformation>
+        </DetailInformation> :
+        <ErrorHandler>{err}</ErrorHandler>}
+    </Container>
+  </Fragment>;
 }
 
 export default Detail;
+
+const Container = styled.div`
+border-top: 1px solid white;
+  height: 80vh;
+  background-color: #000;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DetailInformation = styled.div``;
+
+const ErrorHandler = styled.div`
+  color: white;
+`;
