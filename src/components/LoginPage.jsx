ad975e04d1b84b28497b57e537b963879e915e1b
Nil Andreu <nilandreug@gmail.com>
2021-10-05 18:16:28 +0200

    Initial page2

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const LoginPage = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    height: calc(80vh - 1px);
+    background-color: black;
+    border-top: 1px white solid;
+`;
+
+export default LoginPage
