bab26510ed86437588c8827530a46cc3ef028c48
Nil Andreu <nilandreug@gmail.com>
2021-10-05 18:37:37 +0200

    Going to create the redirect

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import {useHistory,Link} from 'react-router-dom'
 
 const LoginPage = () => {
+    const Submit = () => {
+        // We will redirect when the button is clicked
+        useHistory().push("starships/") 
+    }
     return (
         <Container>
-            
+            <LoginForm>
+                <Button onClick={Submit}></Button>
+            </LoginForm>
         </Container>
     )
 }
@@ -14,6 +20,25 @@ const Container = styled.div`
     height: calc(80vh - 1px);
     background-color: black;
     border-top: 1px white solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
+
+const LoginForm = styled.div`
+    width: 50vw;
+    height: 50vh;
+    background-color: white;
+`;
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+  max-width: 40%;
+  padding: 2rem 8rem;
+  margin-bottom: 20vh;
+  font-family: "Roboto Mono", "Arial";
+`;
+
 
 export default LoginPage
