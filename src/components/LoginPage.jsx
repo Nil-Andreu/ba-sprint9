0fe10e5ea3e58376de62fc50c385f01825705c3e
Nil Andreu <nilandreug@gmail.com>
2021-10-06 18:37:14 +0200

    Exercise 5 done

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import styled from 'styled-components'
-import {useHistory,Link} from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { useHistory, Link } from "react-router-dom";
 
 const LoginPage = () => {
-    /*const Submit = () => {
-        // We will redirect when the button is clicked
-        useHistory().push("starships/") 
-    }*/
-    return (
-        <Container>
-            <LoginForm>
-                <h1>Welcome to the page</h1>
-                <Button to="starships/"></Button>
-            </LoginForm>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <LoginForm>
+        <h1>Welcome to Star Wars App</h1>
+        <Button to="starships/">Enter to this app</Button>
+      </LoginForm>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    height: calc(80vh - 1px);
-    background-color: black;
-    border-top: 1px white solid;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: calc(80vh - 1px);
+  background-color: black;
+  border-top: 1px white solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const LoginForm = styled.div`
-    width: 50vw;
-    height: 50vh;
-    background-color: white;
+  width: 50vw;
+  height: 50vh;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const Button = styled(Link)`
-    background-color: black;
-    color: white;
+  background-color: black;
+  text-decoration: none;
+  color: white;
   max-width: 40%;
   padding: 2rem 8rem;
-  margin-bottom: 20vh;
   font-family: "Roboto Mono", "Arial";
 `;
 
-
-export default LoginPage
+export default LoginPage;
