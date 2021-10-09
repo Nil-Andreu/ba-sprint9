db9d0bb15411d073b0d8f83851f0723564254337
Nil Andreu <nilandreug@gmail.com>
2021-10-09 19:05:54 +0200

    Starships sub component

--- src/components/actorsPage/StarshipsSubDetail.jsx
+++ src/components/actorsPage/StarshipsSubDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styled from "styled-components";
+// This will have the same components as pilots cards from starhsips page
+import {CloseButton, NextButton, ButtonContainer, CircleActivated, CircleDesactivated, ContainerCircles, P, InformationSubContainer, PilotGender, PilotName, InformationContainer, BoxPilots, Container} from "../starshipsPage/PilotsSubDetailComponents"
 
 function StarshipsDetail({ starshipsURLs, StarshipShow }) {
   // We obtain which is the length of the pilot values
@@ -72,122 +73,6 @@ function StarshipsDetail({ starshipsURLs, StarshipShow }) {
   );
 }
 
-const Container = styled.div`
-  background-color: transparent;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BoxPilots = styled.div`
-  width: 50%;
-  height: 50%;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-`;
-
-const InformationContainer = styled.div`
-  height: 30%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const PilotName = styled.h3`
-  display: block;
-  width: 100%;
-`;
-
-const PilotGender = styled.h6`
-  display: block;
-  width: 100%;
-  text-align: center;
-  text-transform: uppercase;
-`;
-
-const InformationSubContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  height: 50%;
-  flex-wrap: wrap;
-`;
-
-const P = styled.p`
-  width: 20vw;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  margin-left: 5vw;
-`;
-
-const ContainerCircles = styled.div`
-  position: absolute;
-  left: 0;
-  width: 30px;
-  top: 15vh;
-  left: 1.5vw;
-  bottom: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  height: 20vh;
-`;
-
-const CircleActivated = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 100%;
-  background-color: black;
-`;
-
-const CircleDesactivated = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 100%;
-  background-color: gray;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`;
-
-const NextButton = styled.button`
-  background-color: transparent;
-  font-family: "Roboto Mono", "Arial";
-  width: 10vw;
-  border: 1px solid black;
-  margin-right: 30px;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const CloseButton = styled.button`
-  width: 10vw;
-  font-family: "Roboto Mono", "Arial";
-  color: white;
-  padding: 1vh 3vw;
-  background-color: black;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
 
 export default StarshipsDetail;
 
