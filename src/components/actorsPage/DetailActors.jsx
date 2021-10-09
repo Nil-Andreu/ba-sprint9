4d12a9a9237793a07b1baa45774e359f9026f587
Nil Andreu <nilandreug@gmail.com>
2021-10-09 19:01:16 +0200

    Detail actors components

--- src/components/actorsPage/DetailActors.jsx
+++ src/components/actorsPage/DetailActors.jsx
@@ -1,30 +1,36 @@
 import React, { useEffect, useState, Fragment } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import StarshipsDetail from "./StarshipsSubDetail"
-import FilmsDetail from "../FilmsSubDetail"
+import StarshipsDetail from "./StarshipsSubDetail";
+import FilmsDetail from "../FilmsSubDetail";
+import {
+  Container,
+  DetailInformation,
+  ContainerInformation,
+  NameInformation,
+  ModelInformation,
+  ButtonHandlerToNormal,
+  NoInfo,
+  ButtonHandlerFilms,
+  ButtonHandlerPilots,
+} from "./DetailActorsComponents";
 
 // For this detail actors we want to render its films and piltos details
 function DetailActors({ i, setIdRendererActors }) {
   const [starshipDetailsShow, setStarshipDetailsShow] = useState(false);
   const [filmDetailsShow, setFilmDetailShow] = useState(false);
 
-
   let starshipState = false;
-  let filmState = false
+  let filmState = false;
   // HANDLING PILOTS
   let starshipsURLs = i.starships; // array of all pilots urls
-  let filmsURLs = i.films
+  let filmsURLs = i.films;
   if (starshipsURLs.length == 0) {
   } else {
     starshipState = true;
   }
 
   if (filmsURLs.length == 0) {
-
   } else {
-    filmState = true
+    filmState = true;
   }
 
   return (
@@ -45,8 +51,18 @@ function DetailActors({ i, setIdRendererActors }) {
             <div>Has a mass of {i.mass}</div>
           </ContainerInformation>
         </DetailInformation>
-        {filmState ? <ButtonHandlerFilms onClick={() => setFilmDetailShow(true)}>See the films</ButtonHandlerFilms> : <NoInfo>There are no films available</NoInfo>}
-        {filmDetailsShow ? <FilmsDetail filmsURLs={filmsURLs} FilmsShow={setFilmDetailShow}/>: ""}
+        {filmState ? (
+          <ButtonHandlerFilms onClick={() => setFilmDetailShow(true)}>
+            See the films
+          </ButtonHandlerFilms>
+        ) : (
+          <NoInfo>There are no films available</NoInfo>
+        )}
+        {filmDetailsShow ? (
+          <FilmsDetail filmsURLs={filmsURLs} FilmsShow={setFilmDetailShow} />
+        ) : (
+          ""
+        )}
         {starshipState ? (
           <ButtonHandlerPilots onClick={() => setStarshipDetailsShow(true)}>
             See the starships!
@@ -71,88 +87,3 @@ function DetailActors({ i, setIdRendererActors }) {
 }
 
 export default DetailActors;
-
-const Container = styled.div`
-  margin: auto;
-  border-top: 1px solid white;
-  height: auto;
-  width: 100vw;
-  background-color: #000;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-`;
-
-const DetailInformation = styled.div`
-  height: 50vh;
-  width: 100vw;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  border-top: 1px solid red;
-  color: white;
-  flex-direction: column;
-`;
-
-const ContainerInformation = styled.div`
-  width: 100vw;
-  display: grid;
-  grid-template-columns: 30% 30%;
-  grid-template-rows: 8vh;
-  justify-items: flex-start;
-  margin-left: 40%;
-`;
-
-const NameInformation = styled.h1`
-  color: white;
-`;
-
-const ModelInformation = styled.h3`
-  text-transform: uppercase;
-  margin-bottom: 3vh;
-`;
-
-const ButtonHandlerToNormal = styled.button`
-  align-self: center;
-  max-width: 40%;
-  margin-bottom: 5vh;
-  padding: 2rem 8rem;
-  background-color: white;
-  color: #000;
-  font-family: "Roboto Mono", "Arial";
-`;
-
-const NoInfo = styled.p`
-  width: 50%;
-  color: white;
-  margin: auto;
-  text-align: center;
-  margin-bottom: 5vh;
-`;
-
-const ButtonHandlerFilms = styled.button`
-  margin: auto;
-  width: 10vw;
-  background-color: transparent;
-  color: white;
-  border: 1px solid white;
-  padding: 3vh 1vw;
-  margin-bottom: 2vh;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const ButtonHandlerPilots = styled.button`
-  margin: auto;
-  width: 10vw;
-  background-color: gray;
-  color: black;
-  padding: 3vh 1vw;
-  margin-bottom: 2vh;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
---
+++ src/components/actorsPage/DetailActorsComponents.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: auto;
+  border-top: 1px solid white;
+  height: auto;
+  width: 100vw;
+  background-color: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+export const DetailInformation = styled.div`
+  height: 50vh;
+  width: 100vw;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-top: 1px solid red;
+  color: white;
+  flex-direction: column;
+`;
+
+export const ContainerInformation = styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 30% 30%;
+  grid-template-rows: 8vh;
+  justify-items: flex-start;
+  margin-left: 40%;
+`;
+
+export const NameInformation = styled.h1`
+  color: white;
+`;
+
+export const ModelInformation = styled.h3`
+  text-transform: uppercase;
+  margin-bottom: 3vh;
+`;
+
+export const ButtonHandlerToNormal = styled.button`
+  align-self: center;
+  max-width: 40%;
+  margin-bottom: 5vh;
+  padding: 2rem 8rem;
+  background-color: white;
+  color: #000;
+  font-family: "Roboto Mono", "Arial";
+`;
+
+export const NoInfo = styled.p`
+  width: 50%;
+  color: white;
+  margin: auto;
+  text-align: center;
+  margin-bottom: 5vh;
+`;
+
+export const ButtonHandlerFilms = styled.button`
+  margin: auto;
+  width: 10vw;
+  background-color: transparent;
+  color: white;
+  border: 1px solid white;
+  padding: 3vh 1vw;
+  margin-bottom: 2vh;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const ButtonHandlerPilots = styled.button`
+  margin: auto;
+  width: 10vw;
+  background-color: gray;
+  color: black;
+  padding: 3vh 1vw;
+  margin-bottom: 2vh;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
